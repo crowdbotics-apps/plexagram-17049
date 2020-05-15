34afed700c70a2686b6bce2839885d1c9714f31a
Crowdbotics <team@crowdbotics.com>
2020-05-15 17:48:30 +0000

    Update models

--- backend/plexagram_17049/wsgi.py
+++ backend/plexagram_17049/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'plexagram_17049.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "plexagram_17049.settings")
 
 application = get_wsgi_application()
